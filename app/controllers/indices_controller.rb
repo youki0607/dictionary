cd43b40b6fef6c568abccefec204f43c018666d1
youki0607 <its_showtime@i.softbank.jp>
2020-11-15 19:53:43 +0900

    検索機能追加

--- app/controllers/indices_controller.rb
+++ app/controllers/indices_controller.rb
@@ -1,4 +1,5 @@
 class IndicesController < ApplicationController
+  before_action :set_index, only:[:edit, :show]
 
   def index
     @indices = Index.all
@@ -12,9 +13,33 @@ class IndicesController < ApplicationController
     Index.create(index_params)
   end
 
+  def destroy
+    index = Index.find(params[:id])
+    index.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    index = Index.find(params[:id])
+    index.update(index_params)
+  end
+
+  def show
+  end
+
+  def search
+    @indices = Index.search(params[:keyword])
+  end
+
   private
   def index_params
     params.require(:index).permit(:name, :image, :explanation)
+  end
+
+  def set_index
+    @index = Index.find(params[:id])
   end
 
 end
--- app/models/index.rb
+++ app/models/index.rb
@@ -1,3 +1,11 @@
 class Index < ApplicationRecord
   validates :explanation, presence: true
+
+  def self.search(search)
+    if search !=""
+      Index.where('explanation LIKE(?)', "%#{search}%")
+    else
+      Index.all
+    end
+  end
 end
